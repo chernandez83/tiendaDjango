9a3598aed225aa83842db79afd24f9921c3729d5
chernandez83 <104525460+chernandez83@users.noreply.github.com>
2023-04-13 17:13:26 -0600

    Search

--- tienda/products/urls.py
+++ tienda/products/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search', views.ProductSearchListView.as_view(), name='search'),
     path('<int:pk>', views.ProductDetailView.as_view(), name='product'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),
 ]
--- tienda/products/views.py
+++ tienda/products/views.py
@@ -5,21 +5,40 @@ from django.views.generic.detail import DetailView
 
 from products.models import Product
 
+
 class ProductListView(ListView):
     template_name = 'index.html'
     queryset = Product.objects.all().order_by('-id')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Listado de Productos'
         # context['products'] = context['product_list']
         return context
 
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         print(context)
+        return context
+
+
+class ProductSearchListView(ListView):
+    template_name = 'products/search.html'
+    
+    def get_queryset(self):
+        return Product.objects.filter(title__icontains=self.query())
+        # return super().get_queryset()
+    
+    def query(self):
+        return self.request.GET.get('q')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
         return context

127c30015b65314e0f610d5cd47efd3b6381058b
chernandez83 <104525460+chernandez83@users.noreply.github.com>
2023-04-14 11:52:21 -0600

    Usuarios

--- tienda/tienda/views.py
+++ tienda/tienda/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from users.models import User
 
 from .forms import RegisterForm
 
